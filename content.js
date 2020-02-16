e1b530f25f0d3cb22f683365b4992225bd682e9b
neuromancer1729 <stomatrix@gmail.com>
2020-02-16 07:38:03 -0700

    fix minor bug

--- content.js
+++ content.js
@@ -3,9 +3,11 @@ var newurl;
 
 console.log(window.document.location.href);
 
+var base_url = 'https://unstoppabledomains.com/api/v1/'
+
 if (pattern.test(window.document.location.href)) // if it matches pattern defined above
 {
-  newurl = viewtext_base_url + encodeURIComponent(window.location.href);
+  newurl = base_url + encodeURIComponent(window.location.href);
   chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
 
 }
