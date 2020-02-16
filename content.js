da976d5bbdd019c47bf3a992ae00fc94c66583c0
neuromancer1729 <stomatrix@gmail.com>
2020-02-16 07:46:09 -0700

    avoid infinite loop

--- content.js
+++ content.js
@@ -1,11 +1,12 @@
-var pattern=/.crypto$/;
+var pattern=/.crypto$|.zil$/;
+var pattern2=/docs.unstoppabledomains.com/;
 var newurl;
 
 console.log(window.document.location.href);
 
 var base_url = 'https://unstoppabledomains.com/api/v1/'
 
-if (pattern.test(window.document.location.href)) // if it matches pattern defined above
+if (pattern.test(window.document.location.href) & !pattern2.test(window.document.location.href)) // if it matches pattern defined above
 {
   newurl = base_url + encodeURIComponent(window.location.href);
   chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
