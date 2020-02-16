a655f29785fac21822bf810ce88a1710c7876ab3
neuromancer1729 <stomatrix@gmail.com>
2020-02-16 07:50:03 -0700

    fix2

--- content.js
+++ content.js
@@ -8,7 +8,6 @@ var base_url = 'https://unstoppabledomains.com/api/v1/'
 
 if (pattern.test(window.document.location.href) & !pattern2.test(window.document.location.href)) // if it matches pattern defined above
 {
-  newurl = base_url + encodeURIComponent(window.location.href);
+  newurl = base_url + window.document.location.href.split('://')[1];
   chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
-
 }
